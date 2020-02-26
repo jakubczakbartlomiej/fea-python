8580d006c4701cb8acee99ad7bfd8738024d7b8e
Bartlomiej Jakubczak <b.jakubczak@altoma.net.pl>
2020-02-26 20:54:21 +0100

    main.py developing

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+
+import preprocessor.preprocessorTruss
+
+def loadInputFile():
+    testFilesList = os.listdir("testfiles")
+    i = 1
+    for specificFile in testFilesList:
+        print(str(i) + ".  " + str(specificFile))
+        i += 1
+    userChoice = input("Which file would you like to use? (insert number and press ENTER): ")
+    return testFilesList[int(userChoice)-1]
+
+def checkElementType(inputFile):
+    fileToCheck = open("testfiles/" + inputFile, 'r')
+    checkLine = ""
+    while(checkLine[0:2] != "ET"):
+        checkLine = fileToCheck.readline()
+    fileToCheck.close()
+    if("LINK180" in checkLine):
+        print("\n***********************************************")
+        print("File loaded successfully! Element type: LINK180")
+        print("***********************************************\n")
+    else:
+        print("Error! Element type incorrect or not found!")
+
+if __name__ == "__main__":
+    inputFile = loadInputFile()
+    checkElementType(inputFile)
+
