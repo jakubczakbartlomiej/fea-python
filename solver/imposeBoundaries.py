e3614d29116cb02e17d19e83b4d400d8d4638adb
Bartlomiej Jakubczak <b.jakubczak@altoma.net.pl>
2020-04-13 16:37:20 +0200

    backup

--- solver/imposeBoundaries.py
+++ solver/imposeBoundaries.py
@@ -2,12 +2,15 @@ import numpy as np
 
 def imposeBoundaryConditions(amount, supports, loadVector, stiffnessMatrix):
     numberOfSupports = amount[3]
+    #print(loadVector)
+    #print("")
     for i in range(0,numberOfSupports):
         if supports[i,2] == "UX":
-            degreeOfFreedom = 2 * int(supports[i,1]) - 1
+            degreeOfFreedom = 2 * int(supports[i,1]) - 2 + 1
         elif supports[i,2] == "UY":
-            degreeOfFreedom = 2 * int(supports[i,1])
-        loadVector[degreeOfFreedom] = float(supports[i,3])
+            degreeOfFreedom = 2 * int(supports[i,1]) - 2 + 2
+        #print(degreeOfFreedom)
+        loadVector[degreeOfFreedom-1] = float(supports[i,3])
         stiffnessMatrix[:,degreeOfFreedom] = 0
         stiffnessMatrix[degreeOfFreedom,:] = 0
         stiffnessMatrix[degreeOfFreedom,degreeOfFreedom] = 1
--- solver/matrixSolver.py
+++ solver/matrixSolver.py
@@ -1,5 +1,9 @@
 import numpy as np 
 
 def calculateDisplacements(loadVector, stiffnessMatrix):
+    print(stiffnessMatrix)
+    print("")
+    print(loadVector)
+    print("")
     nodalDisplacements = np.dot(loadVector, stiffnessMatrix)
     return nodalDisplacements
